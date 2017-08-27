159a0b78bdd5de04a34084aa6df59df957fc1ea5
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2017-08-27 16:36:35 +0000

    Adding in2cpp

---
+++ in2cpp/README.md
@@ -0,0 +1,12 @@
+in2cpp
+============
+Basically turns what's being put in stdin into a cpp array definition.
+
+## Usage
+`in2cpp tabName`
+
+## Installation
+`make`. You can then copy it into `/usr/local/bin` for example.
+
+## Examples
+ * `cat file | in2cpp outputTab > outputFile.inc.hpp`
---
+++ in2cpp/main.cpp
@@ -0,0 +1,43 @@
+// Turns data from stdin into a c++ array header.
+// Usage   : buf2cpp tabName
+// Example : cat file | buf2cpp tabName > tab.inc.hpp
+
+#include <iostream>
+#include <string>
+#include <cstdint>
+
+const char evilTab[] {
+    '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+};
+
+int main(int argc, const char *argv[])
+{
+    std::string name = argc < 2 ? "data" : std::string(argv[1]);
+    std::string NAME = name;
+    for( auto & c : NAME ) c = toupper(c);
+
+    std::cout << "#ifndef HEADER_" << NAME << std::endl << "#define HEADER_" << NAME << std::endl
+              << "#include <cstdint>"     << std::endl
+              << "namespace " << name << std::endl << "{" << std::endl
+              << "    std::uint8_t data[] {"
+    ;
+
+    char  c;
+    std::uint64_t count; //number of bytes in the stream
+    for( count = 0 ; std::cin.get( c ) ; count++ )
+    {
+        if( count ) std::cout << ", ";
+        if( !(count % 10) ) std::cout << std::endl << "        ";
+
+        std::cout << "0x" << evilTab[(c & 0xF0) >> 4] << evilTab[c & 0x0F];
+    }
+
+    std::cout << std::endl << "    };" << std::endl
+              << "    std::uint64_t size = " << count << ";" << std::endl
+              << "}" << std::endl
+              << "#endif" << std::endl
+    ;
+
+    return 0;
+}
+
